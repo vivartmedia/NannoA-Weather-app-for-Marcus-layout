8b905b868eb0e18ca3bd38e2042a55439ddf84dc
vivartmedia <nanno70@yahoo.de>
2023-12-04 22:10:53 -0800

    max min updated

--- scripts/app.js
+++ scripts/app.js
@@ -63,15 +63,15 @@ function ApiCall(lat, lon) {
             let tempDgr = document.getElementById('tempDgr');
             let tempMax = document.getElementById('tempMax');
             let tempMin = document.getElementById('tempMin');
-            let dgrFarenheit = ("The temp now is = " + (Math.round(((data.main.temp) - 273.15) * 9 / 5 + 32)) + " degree fahrenheit!");
+            let dgrFarenheit = ("Current temp " + (Math.round(((data.main.temp) - 273.15) * 9 / 5 + 32)) + "");
             // tempDgr.textContent = dgrFarenheit;
             console.log(dgrFarenheit)
             main12.textContent = dgrFarenheit
-               main2.textContent = ("The MAX temp today is = " + (Math.round(((data.main.temp_max) - 273.15) * 9 / 5 + 32)) + " degree fahrenheit!");
-            main22.textContent =  ("The MIN temp today is = " + (Math.round(((data.main.temp_min) - 273.15) * 9 / 5 + 32)) + " degree fahrenheit!");
+               main2.textContent = ("MAX today " + (Math.round(((data.main.temp_max) - 273.15) * 9 / 5 + 32)) + "");
+            main22.textContent =  (" MIN today" + (Math.round(((data.main.temp_min) - 273.15) * 9 / 5 + 32)) + "");
             
-            console.log("The MAX temp today is = " + (Math.round(((data.main.temp_max) - 273.15) * 9 / 5 + 32)) + " degree fahrenheit!");
-            console.log("The MIN temp today is = " + (Math.round(((data.main.temp_min) - 273.15) * 9 / 5 + 32)) + " degree fahrenheit!");
+            console.log(" MAX  today = " + (Math.round(((data.main.temp_max) - 273.15) * 9 / 5 + 32)) + "");
+            console.log(" MIN today  = " + (Math.round(((data.main.temp_min) - 273.15) * 9 / 5 + 32)) + " ");
             // ("The MAX temp in here is = " +  (Math.round(((data.main.temp_min) - 273.15) * 9 / 5 + 32)) + " degree fahrenheit!");
             // tempMax.textContent = ("The MAX temp in here is = " +  (Math.round(((data.main.temp_max) - 273.15) * 9 / 5 + 32)) + " degree fahrenheit!");
             // tempMin.textContent = ("The MIN temp in here is = " +  (Math.round(((data.main.temp_min) - 273.15) * 9 / 5 + 32)) + " degree fahrenheit!");
@@ -169,7 +169,8 @@ function ApiCall5DaysForcast(lat, lon) {
                 }
             }
             console.log("Max temp for " + (daysDigit(dayOfWeek + 2)) + " is " + (Math.round((maxTempDay2 - 273.15) * (9 / 5) + 32)))
-            
+             Day2Max.textContent = (Math.round((maxTempDay2 - 273.15) * (9 / 5) + 32))
+
             let minTempDay2 = 500;    
             for (let i = 0; i < 39; i++) {
             // minTempDay2= data.list[i].main.temp_min ; 
@@ -182,7 +183,7 @@ function ApiCall5DaysForcast(lat, lon) {
                 }
             }           
             console.log("Min temp for " + (daysDigit(dayOfWeek + 2)) + " is " + (Math.round((minTempDay2 - 273.15) * (9 / 5) + 32)))
-
+             Day2Min.textContent =  (Math.round((minTempDay2 - 273.15) * (9 / 5) + 32))
 
 
 
@@ -197,7 +198,8 @@ function ApiCall5DaysForcast(lat, lon) {
                 }
             }
             console.log("Max temp for " + (daysDigit(dayOfWeek + 3)) + " is " + (Math.round((maxTempDay3 - 273.15) * (9 / 5) + 32)))
-            
+            Day3Max.textContent = (Math.round((maxTempDay3 - 273.15) * (9 / 5) + 32))
+                 
             let minTempDay3 = 500;    
             for (let i = 0; i < 39; i++) {
             // minTempDay3= data.list[i].main.temp_min ; 
@@ -210,7 +212,8 @@ function ApiCall5DaysForcast(lat, lon) {
                 }
             }           
             console.log("Min temp for " + (daysDigit(dayOfWeek + 3)) + " is " + (Math.round((minTempDay3 - 273.15) * (9 / 5) + 32)))
-
+            Day3Min.textContent = (Math.round((minTempDay3 - 273.15) * (9 / 5) + 32))
+                 
 
             
             let maxTempDay4 = 0;
@@ -224,7 +227,8 @@ function ApiCall5DaysForcast(lat, lon) {
                 }
             }
             console.log("Max temp for " + (daysDigit(dayOfWeek + 4)) + " is " + (Math.round((maxTempDay4 - 273.15) * (9 / 5) + 32)))
-
+            Day4Max.textContent = (Math.round((maxTempDay4 - 273.15) * (9 / 5) + 32))
+                
             let minTempDay4 = 500;    
             for (let i = 0; i < 39; i++) {
             
@@ -238,7 +242,8 @@ function ApiCall5DaysForcast(lat, lon) {
                 }
             }           
             console.log("Min temp for " + (daysDigit(dayOfWeek + 4)) + " is " + (Math.round((minTempDay4 - 273.15) * (9 / 5) + 32)))
-            
+            Day4Min.textContent = (Math.round((minTempDay4 - 273.15) * (9 / 5) + 32))
+                 
 
             let maxTempDay5 = 0;
             for (let i = 0; i < 39; i++) {
@@ -251,7 +256,8 @@ function ApiCall5DaysForcast(lat, lon) {
                 }
             }
             console.log("Max temp for " + (daysDigit(dayOfWeek + 5)) + " is " + (Math.round((maxTempDay5 - 273.15) * (9 / 5) + 32)))
-
+            Day5Max.textContent = (Math.round((maxTempDay5 - 273.15) * (9 / 5) + 32))
+                 
             let minTempDay5 = 500;    
             for (let i = 0; i < 39; i++) {
             
@@ -268,6 +274,7 @@ function ApiCall5DaysForcast(lat, lon) {
             }           
             console.log("Min temp for " + (daysDigit(dayOfWeek + 5)) + " is " + (Math.round((minTempDay5 - 273.15) * (9 / 5) + 32)))
             // console.log("Min temp for " + (daysDigit(dayOfWeek + 5)) + " is " + minTempDay5)
+            Day5Min.textContent = (Math.round((minTempDay5 - 273.15) * (9 / 5) + 32))
 
 
 
@@ -281,9 +288,9 @@ main22.textContent = daysDigit(dayOfWeek )
 
 dayName1.textContent = daysDigit(dayOfWeek + 1)
 dayName2.textContent = daysDigit(dayOfWeek + 2)
-day3.textContent = daysDigit(dayOfWeek + 3)
-day4.textContent = daysDigit(dayOfWeek + 4)
-day5.textContent = daysDigit(dayOfWeek + 5)
+dayName3.textContent = daysDigit(dayOfWeek + 3)
+dayName4.textContent = daysDigit(dayOfWeek + 4)
+dayName5.textContent = daysDigit(dayOfWeek + 5)
 // ApiCall5DaysForcast();
 function ApiCallLocation(lat, lon) {
 
