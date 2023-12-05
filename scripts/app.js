d708ea55cb13c9f94356bb0703395f3baf9653ec
vivartmedia <nanno70@yahoo.de>
2023-12-04 16:51:32 -0800

    converted all 5 days max and min temperatures to F

--- scripts/app.js
+++ scripts/app.js
@@ -89,22 +89,151 @@ function ApiCall5DaysForcast(lat, lon) {
         })
         .then((data) => {
             console.log(data)
-            console.log(data.list[0].main.temp_max)
+            // console.log(data.list[0].main.temp_max)
             console.log(data.list[0].dt_txt[9])
             console.log(`${dd}`);
-            let j = 0;
+     
+            let maxTempDay1= 0;           
             for (let i = 0; i < 11; i++) {
                 if ((data.list[i].dt_txt[9]) == dd + 1) {
-                    j = data.list[i].main.temp_max ;
-                    j++;
-                    data.list[i].main.temp_max += 0;
-                    // console.log("Max temp for day " + (dd +1)+ " is " )
+                    // console.log(data.list[i].main.temp_max)
+                    if (data.list[i].main.temp_max > maxTempDay1) { 
+                    maxTempDay1 = data.list[i].main.temp_max;
+                    // console.log(i)
                 }
-                // else {
-                //     console.log("todays date is not: ")
-                // }
+                }
+            }           
+            console.log("Max temp for " + (daysDigit(dayOfWeek + 1)) + " is " + (Math.round((maxTempDay1 - 273.15) * (9 / 5) + 32)))
+
+            let minTempDay1 = 500;    
+            for (let i = 0; i < 11; i++) {
+            // minTempDay1= data.list[i].main.temp_min ; 
+                if ((data.list[i].dt_txt[9]) == dd + 1) {
+                    // console.log(data.list[i].main.temp_max)
+                    if (data.list[i].main.temp_min < minTempDay1) { 
+                    minTempDay1 = data.list[i].main.temp_min;
+                    // console.log(i)
+                }
+                }
+            }           
+            console.log("Min temp for " + (daysDigit(dayOfWeek + 1)) + " is " + (Math.round((minTempDay1 - 273.15) * (9 / 5) + 32)))
+
+
+
+            
+            let maxTempDay2= 0;
+            for (let i = 0; i < 39; i++) {
+                if ((data.list[i].dt_txt[9]) == dd + 2) {
+                    // console.log(data.list[i].main.temp_max)
+                    if (data.list[i].main.temp_max > maxTempDay2) { 
+                    maxTempDay2 = data.list[i].main.temp_max;
+                    // console.log(i)
+                }
+                }
             }
-            console.log("Max temp for day " + (daysDigit(dayOfWeek+1)) + " is " + (Math.round((j  - 273.15) * 9 / 5 + 32)))
+            console.log("Max temp for " + (daysDigit(dayOfWeek + 2)) + " is " + (Math.round((maxTempDay2 - 273.15) * (9 / 5) + 32)))
+            
+            let minTempDay2 = 500;    
+            for (let i = 0; i < 39; i++) {
+            // minTempDay2= data.list[i].main.temp_min ; 
+                if ((data.list[i].dt_txt[9]) == dd + 2) {
+                    // console.log(data.list[i].main.temp_max)
+                    if (data.list[i].main.temp_min < minTempDay2) { 
+                    minTempDay2 = data.list[i].main.temp_min;
+                    // console.log(i)
+                }
+                }
+            }           
+            console.log("Min temp for " + (daysDigit(dayOfWeek + 2)) + " is " + (Math.round((minTempDay2 - 273.15) * (9 / 5) + 32)))
+
+
+
+
+            let maxTempDay3 = 0;
+            for (let i = 0; i < 39; i++) {
+                if ((data.list[i].dt_txt[9]) == dd + 3) {
+                    // console.log(data.list[i].main.temp_max)
+                    if (data.list[i].main.temp_max > maxTempDay3) { 
+                    maxTempDay3 = data.list[i].main.temp_max;
+                    // console.log(i)
+                }
+                }
+            }
+            console.log("Max temp for " + (daysDigit(dayOfWeek + 3)) + " is " + (Math.round((maxTempDay3 - 273.15) * (9 / 5) + 32)))
+            
+            let minTempDay3 = 500;    
+            for (let i = 0; i < 39; i++) {
+            // minTempDay3= data.list[i].main.temp_min ; 
+                if ((data.list[i].dt_txt[9]) == dd + 3) {
+                    // console.log(data.list[i].main.temp_max)
+                    if (data.list[i].main.temp_min < minTempDay3) { 
+                    minTempDay3 = data.list[i].main.temp_min;
+                    // console.log(i)
+                }
+                }
+            }           
+            console.log("Min temp for " + (daysDigit(dayOfWeek + 3)) + " is " + (Math.round((minTempDay3 - 273.15) * (9 / 5) + 32)))
+
+
+            
+            let maxTempDay4 = 0;
+            for (let i = 0; i < 39; i++) {
+                if ((data.list[i].dt_txt[9]) == dd + 4) {
+                    // console.log(data.list[i].main.temp_max)
+                    if (data.list[i].main.temp_max > maxTempDay4) { 
+                    maxTempDay4 = data.list[i].main.temp_max;
+                    // console.log(i)
+                }
+                }
+            }
+            console.log("Max temp for " + (daysDigit(dayOfWeek + 4)) + " is " + (Math.round((maxTempDay4 - 273.15) * (9 / 5) + 32)))
+
+            let minTempDay4 = 500;    
+            for (let i = 0; i < 39; i++) {
+            
+                if ((data.list[i].dt_txt[9]) == dd + 4) {
+                    // minTempDay4= data.list[i].main.temp_min ; 
+                    // console.log(data.list[i].main.temp_max)
+                    if (data.list[i].main.temp_min < minTempDay4) { 
+                    minTempDay4 = data.list[i].main.temp_min;
+                    // console.log(i)
+                }
+                }
+            }           
+            console.log("Min temp for " + (daysDigit(dayOfWeek + 4)) + " is " + (Math.round((minTempDay4 - 273.15) * (9 / 5) + 32)))
+            
+
+            let maxTempDay5 = 0;
+            for (let i = 0; i < 39; i++) {
+                if ((data.list[i].dt_txt[9]) == dd + 5) {
+                    // console.log(data.list[i].main.temp_max)
+                    if (data.list[i].main.temp_max > maxTempDay5) { 
+                    maxTempDay5 = data.list[i].main.temp_max;
+                    // console.log(i)
+                }
+                }
+            }
+            console.log("Max temp for " + (daysDigit(dayOfWeek + 5)) + " is " + (Math.round((maxTempDay5 - 273.15) * (9 / 5) + 32)))
+
+            let minTempDay5 = 500;    
+            for (let i = 0; i < 39; i++) {
+            
+                if ((data.list[i].dt_txt[9]) == dd + 5) {
+                    // minTempDay5= data.list[i].main.temp_min ; 
+                    // console.log(data.list[i].main.temp_max)
+                    if (data.list[i].main.temp_min < minTempDay5) { 
+                    minTempDay5 = data.list[i].main.temp_min;
+                        // console.log(i)
+                        // console.log(minTempDay5)
+                    }
+                    
+                }
+            }           
+            console.log("Min temp for " + (daysDigit(dayOfWeek + 5)) + " is " + (Math.round((minTempDay5 - 273.15) * (9 / 5) + 32)))
+            // console.log("Min temp for " + (daysDigit(dayOfWeek + 5)) + " is " + minTempDay5)
+
+
+
         })
 }
 
